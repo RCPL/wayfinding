c766fdc800cd2545703fb750fc224547a18ca107
Wray Bowling <wbowling@richlandlibrary.com>
2018-01-24 14:03:48 -0500

    Wow. Variable fonts actually work in Chrome.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
 	<div>
-		<h1>An update for this screen is in progress.</h1>
+		<h1 style="padding:100px">An update for this screen is in progress.</h1>
     <Spaces />
 	</div>
 </template>
@@ -19,24 +19,27 @@
   setTimeout("location.reload(true);",120000);
 </script>
 
-<style>
-	*{
-		font-family:inherit;
-	}
+<style lang="scss">
+	// *{
+	// 	font-family:inherit;
+	// }
 	@font-face {
 			font-family: "Avenir Next Variable";
-			src: url("../static/font/AvenirNext_Variable.ttf") format("truetype");
+			src: url("../static/variable-fonts/AvenirNext/AvenirNext_Variable.ttf") format("truetype");
+	}
+
+	body{
+
+		font-family: "Avenir Next Variable";//, Helvetica, Arial, sans-serif;
+		color: rgb(0, 0, 0);
+    font-size: 48px;
+    font-variation-settings: 'wght' 631.164, 'wdth' 188.6799;
+
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
 	}
 	
 	#app {
-		font-family: 'Avenir Next Variable', Helvetica, Arial, sans-serif;
-		color: rgb(0, 0, 0);
-    font-size: 148px;
-    font-variation-settings: 'wght' 631.164, 'wdth' 88.6799;
-		-webkit-font-smoothing: antialiased;
-		-moz-osx-font-smoothing: grayscale;
-		text-align: center;
-		color: #2c3e50;
-		margin-top: 60px;
+		
 	}
 </style>
