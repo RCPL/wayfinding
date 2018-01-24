e43bb36fbe4956bd233eed0d0cc54b1011aeb23c
Wray Bowling <wbowling@richlandlibrary.com>
2018-01-24 13:52:53 -0500

    Fixed Vue version, added variable font submodule

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+	<div>
+		<h1>An update for this screen is in progress.</h1>
+    <Spaces />
+	</div>
+</template>
+
+<script type="text/babel">
+  // import FloorMaps from './components/FloorMaps'
+  import Spaces from './components/Spaces'
+
+  export default {
+    componenets: {
+      // FloorMaps,
+      Spaces
+    }
+  }
+
+  setTimeout("location.reload(true);",120000);
+</script>
+
+<style>
+	*{
+		font-family:inherit;
+	}
+	@font-face {
+			font-family: "Avenir Next Variable";
+			src: url("../static/font/AvenirNext_Variable.ttf") format("truetype");
+	}
+	
+	#app {
+		font-family: 'Avenir Next Variable', Helvetica, Arial, sans-serif;
+		color: rgb(0, 0, 0);
+    font-size: 148px;
+    font-variation-settings: 'wght' 631.164, 'wdth' 88.6799;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+	}
+</style>
---
+++ src/components/Spaces.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="spaces">
+    <h1>Spaces</h1>
+    <p>this is a test of nested content</p>
+  </section>
+</template>
+
+<script type="text/babel">
+export default {
+  name: 'Spaces',
+  data() {
+    
+  }
+}
+</script>
