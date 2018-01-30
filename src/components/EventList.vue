5c89df2a27f35e116c48e027c8e2603f15c15897
Wray Bowling <wbowling@richlandlibrary.com>
2018-01-30 10:33:56 -0500

    repurposed code from events page prototype to clean up evanced data.

--- src/components/EventList.vue
+++ src/components/EventList.vue
@@ -1,12 +1,15 @@
 <template>
   <div>
+    <section v-for="event in events" :key="event.id" class="eventItem">
+      <div class="times">
+        <h2 class="big">{{event.time}}</h2>
+        <h3 class="little">til {{event.endtime}}</h3>
+      </div>
 
-    <section v-for="event in events" :key="event.id._text">
-      <h2>{{event.title[0]}}</h2>
-
-      <h3>{{event.time[0]}} til {{event.endtime[0]}}</h3>
-      <!-- <h3>{{event.space}}</h3> -->
-      <!-- <p>{{event.library[0]}}</p> -->
+      <div class="about">
+        <h2>{{event.title}}</h2>
+        <h3>{{event.location}}</h3>
+      </div>
     </section>
   </div>
 </template>
@@ -14,15 +17,16 @@
 <script>
   import _ from 'underscore'
   import xml2js from 'xml2js'
+
   async function getList() {
     // https://stackoverflow.com/a/41009103
-    const url = 'https://host6.evanced.info/richland/evanced/eventsxml.asp?lib=all&nd=1&alltime=1&dm=exml'
+    const url = 'https://host6.evanced.info/richland/evanced/eventsxml.asp?lib=all&nd=8&alltime=1&dm=exml'
     let xmlString = await fetch(url)
     .then(response => response.text())
 
     const options = {trim:true, explicitArray:false, emptyTag:undefined}
     let eventArray = {}
-    xml2js.parseString(xmlString, (error,result) => {
+    xml2js.parseString(xmlString, options, (error,result) => {
       if(error){
         console.error('OMG',error);
       }else{
@@ -30,7 +34,22 @@
       }
     })
 
-    eventArray = _.filter(eventArray, event => event.library[0] == "Richland Library  Main ")
+    // just main
+    eventArray =  _.where(eventArray, {library: "Richland Library  Main"})
+
+    // clean the data so that it's actually useful
+    _.each(eventArray, eventItem => {
+      if(eventItem.time === "All Day"){
+        eventItem.iso_start = moment(new Date(eventItem.date)).startOf('day')._d;
+        eventItem.iso_end = moment(new Date(eventItem.date)).endOf('day')._d;
+      }else{
+        eventItem.iso_start = new Date(eventItem.date + ' ' + eventItem.time);
+        eventItem.iso_end = new Date(eventItem.date + ' ' + eventItem.endtime);
+      }
+      eventItem.eventtypes = _.without(eventItem.eventtypes.split(', '),',',' ','  ');
+      eventItem.agegroups = _.without(eventItem.agegroups.split(', '),',','',' ','  ');
+      if(eventItem.signup == 1) eventItem.signup_openings = Math.floor(Math.random()*30);
+    });
 
     return eventArray
   }
@@ -46,7 +65,7 @@
       saveList: function() {
         getList().then((newList) => {
           this.events = newList
-          console.log(newList);
+          console.log('newList',newList);
         })
       }
     },
@@ -58,5 +77,8 @@
 </script>
 
 <style lang="scss" scoped>
+  .eventItem{
+    display:flex;
+    flex-direction: row;
+  }
 </style>
-
